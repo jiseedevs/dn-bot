bdb04114cda47c5f39ae1da481dcd82fd6b95ad6
jiseedevs <jisee.devs@gmail.com>
2023-06-23 11:12:08 +0800

    update adding of default url to check if there's no record in database.

--- bot.py
+++ bot.py
@@ -25,68 +25,89 @@ class DiscordCommand:
 
     def set_is_url_for_dn(self, is_for_dn):
         self.is_for_dn = is_for_dn
-    
+
     async def setup_hook(self):
         if self.is_ready:
             await self.schedule_tasks.start()
 
-    @tasks.loop(minutes=30.0)
+    @tasks.loop(seconds=5.0)
     async def schedule_tasks(self, guild):
 
-        for url in self.db.get_all_url():
-            id, username, link, target, html, encrypted, date_added = url
-            response = self.controls.get(link)
-            if response:
+        if self.db.get_all_url():
 
-                new_html = str(self.controls.find_target(response.text, 'table', _class='bbs_list', multiple=False))
+            for url in self.db.get_all_url():
+                id, username, link, target, html, encrypted, date_added = url
+                response = self.controls.get(link)
+                if response:
 
-                old_html = str(self.controls.find_target(html, 'table', _class='bbs_list', multiple=False))
+                    new_html = str(self.controls.find_target(response.text, 'table', _class='bbs_list', multiple=False))
 
-                hashed = self.controls.convert(bytes(new_html, encoding='utf-8')).hexdigest()
+                    old_html = str(self.controls.find_target(html, 'table', _class='bbs_list', multiple=False))
 
-                if hashed != encrypted:
-                    new = self.controls.remove_whitespace(new_html)
-                    old = self.controls.remove_whitespace(old_html)
-                    comparison = self.controls.compare(new, old)
+                    hashed = self.controls.convert(bytes(new_html, encoding='utf-8')).hexdigest()
 
-                    for item in comparison:
-                        # Check the changes returned by the compare method which is marked with "-" as starting of the string.
+                    if hashed != encrypted:
 
-                        if item.startswith('-'):
-                            # Used to get the id of th post that will be used to build the url.
-                            match = self.controls.find(item)
+                        # Store all created links here
+                        links = []
 
-                            # This logic is tailored for DN website which has 4 sequence digit that act as an id for their news updates. 
-                            if match and len(match.group(0)) >= 4 and guild:
-                                id = match.group(0)
+                        # Get text channels.
+                        channels = guild[0].text_channels
 
-                                channels = guild[0].text_channels
 
-                                for channel in channels:
-                                    if channel.name == 'dn-news' and self.is_for_dn:
-                                        await channel.send(
-                                            f'Update!\n{self.controls.build_url(f"{link}/all", id)}'
-                                        )
+                        new = self.controls.remove_whitespace(new_html)
+                        old = self.controls.remove_whitespace(old_html)
+                        comparison = self.controls.compare(new, old)
 
-                                        # Update the current HTML & encrypted(hash) record in the database with the newest updates.
-                                        record = {
-                                            'username': 'jiseoh',
-                                            'url': link,
-                                            'html': new_html,
-                                            'encrypted': hashed,
-                                        }
-                                        self.db.update_record(record)
-                                        await channel.send('Reference has been updated successfully.')
-                                        print(f'{self.controls.build_url(f"{link}/all", id)}')
+                        for item in comparison:
+                            # Check the changes returned by the compare method which is marked with "-" as starting of the string.
 
-                                    elif channel.name:
-                                        # Ready logic for not dn related stuff.
-                                        pass
+                            if item.startswith('-'):
+                                # Used to get the id of th post that will be used to build the url.
+                                match = self.controls.find(item)
 
-                                    else:
-                                        continue
-                else:
-                    print('Same hash.')
+                                # This logic is tailored for DN website which has 4 sequence digit that act as an id for their news updates.
+                                if match and len(match.group(0)) >= 4 and guild:
+
+                                    url_id = match.group(0)
+
+                                    for channel in channels:
+
+                                        if channel.name == 'dn-news' and self.is_for_dn:
+                                            created_url = self.controls.build_url(f"{link}/all", url_id)
+
+                                            if created_url not in links:
+                                                links.append(created_url)
+
+                                            for url in links:
+
+                                                await channel.send(
+                                                    f'Update!\n{url}'
+                                                )
+
+                            # Update the current HTML & encrypted(hash) record in the database with the newest updates.
+
+                            record = {
+                                'username': 'jiseoh',
+                                'url': link,
+                                'html': new_html,
+                                'encrypted': hashed,
+                            }
+
+                            self.db.update_record(record)
+
+                    else:
+                        print('Same hash.')
+        else:
+
+            record = {
+                        'username': 'jiseoh',
+                        'url': 'https://sea.dragonnest.com/news/notice',
+                        'html': '',
+                        'target': '',
+                        'encrypted': '',
+                    }
+            self.db.insert_to_webpage(record)
 
 if __name__ == '__main__':
     intents = discord.Intents.default()
